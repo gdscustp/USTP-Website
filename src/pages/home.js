03f42ae5a1fd2aae85f53d67a80f4e6fbc753403
Kent <65537922+kntjspr@users.noreply.github.com>
2024-09-22 17:10:12 +0800

    Added custom fonts and other css improvements
    - Added gradient on homepage
    - Added local google fonts
    - Adjusted navbar font styles
    - Added default fonts on homepage
    
    	new file:   src/assets/fonts/GoogleSans-Bold.ttf
    	new file:   src/assets/fonts/GoogleSans-BoldItalic.ttf
    	new file:   src/assets/fonts/GoogleSans-Italic.ttf
    	new file:   src/assets/fonts/GoogleSans-Medium.ttf
    	new file:   src/assets/fonts/GoogleSans-MediumItalic.ttf
    	new file:   src/assets/fonts/GoogleSans-Regular.ttf
    	modified:   src/components/navBar.css
    	modified:   src/components/navBar.js
    	modified:   src/pages/home.css
    	modified:   src/pages/home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -18,8 +18,8 @@ export default function Home() {
                 <header className="banner">
 
 
-
-                    <h1>Building Good Things, Together!</h1>
+            
+                    <h1>Building Good Things,&nbsp;<span id="gradient">Together!</span></h1>
                     <p>Rorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
                     <button>
