0e22c018747378956aa1af5a20610b6b3449538b
Kent <65537922+kntjspr@users.noreply.github.com>
2024-09-30 21:36:23 +0800

    fetch to main

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './footer.css'
 
 export default function Footer() {
@@ -15,3 +16,16 @@ export default function Footer() {
         </footer>
     )
 }
+=======
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer className='footer'>        
+            <div className='lower'>
+                <p>&copy; {new Date().getFullYear()} GDSC USTP. -Univeristy of Science and Technology of Southern Philippines - CDO</p>
+            </div>
+        </footer>
+    );
+}
+>>>>>>> 8df0d8f447253b5d8401c21e1ad36cdde8b4003d
