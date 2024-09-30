b6e767bf897911190fbce3828ffefd437b84e5d0
Bryll Mantatao <86003358+iRyllB@users.noreply.github.com>
2024-09-30 20:51:13 +0800

    Updated Footers

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,12 @@
-import './footer.css'
+import './footer.css';
+import { FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa';
 
 export default function Footer() {
     return (
-        <footer className='footer'>
-           <div className='upper'>
-
-           </div>
-
-           <div className='lower'>
-
-           </div>
+        <footer className='footer'>        
+            <div className='lower'>
+                <p>&copy; {new Date().getFullYear()} GDSC USTP. -Univeristy of Science and Technology of Southern Philippines - CDO</p>
+            </div>
         </footer>
-    )
+    );
 }
